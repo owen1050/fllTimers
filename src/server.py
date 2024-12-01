6b1caa9e2a2b108dd923794c870402e3941ad68f
Owen Busler <owen1050@gmail.com>
2024-12-01 17:33:54 -0500

    init

---
+++ src/server.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import request
+from threading import Thread
+
+
+import json, time, requests
+
+db = databaseQuerys()
+app = Flask(__name__, static_folder='static', static_url_path='')
+
+
+@app.route('/')
+def main():
+	return app.send_static_file('timer.html')
+
+@app.route('/matchtimer')
+def matchTimer():
+	return app.send_static_file('matchTime.html')
+
+
+if __name__ == '__main__':
+	global matchTime
+	global matchNum
+	matchNum = 0
+	matchTime = -1
+	app.run(host="0.0.0.0", port=5000)
