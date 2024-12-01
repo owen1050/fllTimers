0fc0417d00b6aed68e60ee3a828178d527e19604
Owen Busler <owen1050@gmail.com>
2024-12-01 17:45:01 -0500

    Update server.py

--- src/server.py
+++ src/server.py
@@ -15,6 +15,57 @@ def matchTimer():
 	return app.send_static_file('matchTime.html')
 
 
+#api endpoints
+@app.route('/api/getMatchTime')
+def apiGetMatchTime():
+	global matchTime
+	print(matchTime)
+	if(matchTime == -1):
+		return str(-1)
+	r = time.time() - matchTime
+	print(r)
+	return str(int(r))
+
+@app.route('/api/resetMatchTime')
+def apiResetMatchTime():
+	global matchTime
+	matchTime = -1
+	return str(0)
+
+@app.route('/api/startMatch')
+def apiStartMatch():
+	global matchTime
+	matchTime  = time.time()
+	print(matchTime)
+	return str(0)
+
+
+@app.route('/api/endMatch')
+def apiEndMatch():
+	global matchTime
+	matchTime = -1
+	return str(0)
+
+
+@app.route('/api/addOneMatchNum')
+def apiAddOneMatchNum():
+	global matchNum
+	matchNum = matchNum + 1
+	return str(0)
+
+@app.route('/api/subOneMatchNum')
+def apiSubOneMatchNum():
+	global matchNum
+	matchNum = matchNum - 1
+	return str(0)
+
+@app.route('/api/getMatchNum')
+def apiGetMatchNum():
+	global matchNum
+	return str(matchNum)
+
+
+
 if __name__ == '__main__':
 	global matchTime
 	global matchNum
